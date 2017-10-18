8824944d631638d9e9c395d0f607cee005f96cd8
Mauricio Mercado <mau@maumercado.com>
2017-10-17 23:16:27 -0700

    Add passport for jwt authentication handling
    Add passport initial Strategy for jwt

---
+++ services/passport.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/user');
+const config = require('../config');
+
+// Setup options for JWT Strategy
+const JWTOptions = {};
+
+// Create JWT Strategy
+const JWTLogin = new JWTStrategy(JWTOptions, async (payload, done) => {
+    try {
+        const user = await User.findById(payload.sub).exec();
+        if (!user) return done(null, false);
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
+});
+
+// Tell passport to use this strategy
